8ffec1d77777da791a43d6a2f3885f673ecef8c8
Nasif <nasifn@gmail.com>
2014-01-10 09:21:08 +0530

    committing renderer intial

--- src/com/tavant/mobilecoe/renderer/GameView.java
+++ src/com/tavant/mobilecoe/renderer/GameView.java
@@ -5,10 +5,14 @@ import android.opengl.GLSurfaceView;
 
 public class GameView extends GLSurfaceView {
 
+	private GameRendrer renderer;
+	
 	public GameView(Context context) {
 		super(context);
 		setEGLContextClientVersion(2);
 		setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+		renderer=new GameRendrer();
+		setRenderer(renderer);
 		
 	}
 
