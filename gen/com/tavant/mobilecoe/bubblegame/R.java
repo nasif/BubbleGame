d897d36cb65939488853ace59b95c309a88f100c
nasif <nasifn@gmail.com>
2014-01-10 13:21:22 +0530

    "configured opengl context"

--- gen/com/tavant/mobilecoe/bubblegame/R.java
+++ gen/com/tavant/mobilecoe/bubblegame/R.java
@@ -25,9 +25,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button_grid=0x7f070002;
+        public static final int gameView=0x7f070001;
         public static final int parent=0x7f070000;
-        public static final int surface=0x7f070001;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
--- src/com/tavant/mobilecoe/bubblegame/FullscreenActivity.java
+++ src/com/tavant/mobilecoe/bubblegame/FullscreenActivity.java
@@ -1,9 +1,15 @@
 package com.tavant.mobilecoe.bubblegame;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Window;
-import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.tavant.mobilecoe.renderer.GameView;
 
 /**
  * 
@@ -13,10 +19,24 @@ import android.view.WindowManager;
 
 public class FullscreenActivity extends Activity {
 
+	private GameView eaglContext=null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_fullscreen);	
+		setContentView(R.layout.activity_fullscreen);
+		eaglContext=(GameView)findViewById(R.id.gameView);		
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		eaglContext.onPause();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		eaglContext.onResume();
 	}
 }
